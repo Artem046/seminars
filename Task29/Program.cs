46a2c4da4977cff92c10d8ddb689dc70ff2fb22b
Artem046 <125895186+Artem046@users.noreply.github.com>
2023-03-11 19:50:13 +0300

    Add files via upload

---
+++ Task29/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] arr = new int[8];
+
+void fillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($" {array[i]}");
+    }
+}
+
+System.Console.WriteLine("ВНИМАНИЕ! После нажатия на кнопку ввода, программа создаст массив из случайных 8ми цифр в диапазоне 0-99!");
+Console.ReadLine();
+
+fillArray(arr);
+printArray(arr);
